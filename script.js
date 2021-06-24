71d17f9b4d79ec0fdc322017758084cbdc15b02c
twizard <trevcoons@gmail.com>
2021-06-23 18:50:23 -0700

    add length prompting function

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function lengthPrompt(){
+  var length = 0;
+  var meetsConditions = false;
+
+  do {
+    length = prompt("How many characters would you like your password to contain?", "Length must be 8 to 128 characters");
+
+    if (length === null) {
+      break;
+     } else if (length >= 8 && length <= 128) {
+              meetsConditions = true;
+            }
+} while (!meetsConditions);
+
+  return length;
+}
